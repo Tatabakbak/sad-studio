173a122d88b66fe39d1bbf2a069035a28b10fdb7
Tata <tats.dev@gmail.com>
2019-11-20 22:09:56 +0300

    switched to react

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './app.scss';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <div>App</div>
+            </div>
+        );
+    }
+};
+
